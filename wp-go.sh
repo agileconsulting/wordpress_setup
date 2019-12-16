c9be9249851038647fa37cafa3233b05a9e3169f
agileconsulting <agileconsulting02@gmail.com>
2019-12-16 11:25:06 +0100

    Update wp-go.sh
    Resolved somo problem with mysql connection

--- wp-go.sh
+++ wp-go.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-#########################################################
-#                                                       #
-#       Package Name: Lamp                              #
-#       Author:Marco Baraldiam                          #
-#       Description: LampPack allows you to             #
-#       automate the installation of LAMP stack         #
-#       and many other utilities on Ubuntu              #
-#                                                       #
-#########################################################
-
-source ./passwd.sh
-directoryName="/var/www/$domainname"
-
-
 #Configure apache
 sudo mkdir $directoryName
 sudo echo "<VirtualHost *:80>
@@ -34,16 +18,17 @@ sudo chown -R www-data:www-data $directoryName
 
 
 # Create database and database user for WordPress
-mysql -u root -p$ROOT_PASS<<EOF
-CREATE DATABASE  $dbname;
-CREATE USER '$dbusername'@'localhost' IDENTIFIED BY '$dbpassword';
-GRANT ALL PRIVILEGES ON *.* TO '$dbusername'@'localhost';
+# Create database and database user for WordPress
+mysql -u root -p$MYSQL_ROOT_PASSWORD<<EOF
+CREATE USER '$WP_DB_USERNAME'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
+CREATE DATABASE $WP_DB_NAME;
+GRANT ALL ON $WP_DB_NAME.* TO '$WP_DB_USERNAME'@'localhost';
+alter user '$WP_DB_USERNAME'@'localhost' identified with mysql_native_password by '$WP_DB_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
 cd $directoryName
 
 sudo wp core download --allow-root 
-sudo wp core config --dbname=$dbname --dbuser=$dbusername --dbpass=$dbpassword --allow-root 
+sudo wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USERNAME --dbpass=$WP_DB_PASSWORD --allow-root 
 sudo wp core install --allow-root --url="$domainname" --title="$sitetitle" --admin_user="$siteuser" --admin_password="$sitepassword" --admin_email="$siteemail"
-

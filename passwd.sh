9233a67de4315c01a8cdaa84d1d24f748ce9f052
agileconsulting <agileconsulting02@gmail.com>
2019-12-28 19:05:26 +0100

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,3 +1,4 @@
+#This change according to every site you will create
 domainname="oopluto"
 sitetitle="Mio titolo"
 siteuser="admin"
@@ -5,7 +6,9 @@ sitepassword="paperoga1"
 WP_DB_USERNAME="rres"
 WP_DB_PASSWORD="wordpress1"
 WP_DB_NAME="wodpess"
-MYSQL_ROOT_PASSWORD="root"
 WP_ADMIN_USERNAME="admin"
 WP_ADMIN_PASSWORD="admin"
 WP_ADMIN_EMAIL="no@spam.org"
+
+#This is valid for each site
+MYSQL_ROOT_PASSWORD="root"
